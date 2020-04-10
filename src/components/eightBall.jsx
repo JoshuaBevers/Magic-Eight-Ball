223ff2ca4841bcf60e0bb542a48057c4256003b6
infernezor@gmail.com <infernezor@gmail.com>
2020-04-10 13:52:25 -0400

    updated eightball.

--- src/components/eightBall.jsx
+++ src/components/eightBall.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { addQnA } from "../redux/actions";
 
-const EightBall = ({ QnA, addQnA }) => {
+const EightBall = ({ QnA }) => {
   const QnAArray = Object.keys(QnA);
-
-  const fetchAnswer = async (question, e) => {
-    e.preventDefault();
-    const fetched = await fetch(
-      ` https://8ball.delegator.com/magic/JSON/${question}`
-    );
-    console.log(fetched);
-    return null;
-  };
 
   return (
     <div>
-      <>Tom Nook Here!</>
-      <div>the input of question and output of answer goes here.</div>
       <div>The list of all the questions and their answers goes here.</div>
       {QnAArray.map((QnAId, index) => {
         return (
@@ -36,4 +24,4 @@ const mapStateToProps = (state) => {
   return QnA;
 };
 
-export default connect(mapStateToProps, { addQnA })(EightBall);
+export default connect(mapStateToProps)(EightBall);
